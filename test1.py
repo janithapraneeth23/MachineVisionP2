ac2c8ce646e7e60e48ab643ce289ec56c60fb377
DESKTOP-91SKKR3\Janitha <janithapraneeth23@gmail.com>
2017-03-21 20:20:20 +0600

    no message

--- test1.py
+++ test1.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 img = cv2.imread('E:/Academics/OpenCV/MachineVissionP2/hotel.seq0.png',cv2.IMREAD_GRAYSCALE)
-cv2.namedWindow("image",cv2.WINDOW_NORMAL)  
+cv2.namedWindow("image",cv2.WINDOW_AUTOSIZE)  
 
 
-dst  = cv2.cornerHarris(img,2,3,0.04)
+dst  = cv2.cornerHarris(img,2,5,0.04)
 
 dst = cv2.dilate(dst,None)
-
-img[dst>0.01 * dst.max()] = [0]
+print(dst)
+img[dst>0.02 * dst.max()] = [255]
 
 cv2.imshow('image',img)  
 
